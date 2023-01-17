5fa59c8eadf2bd7b1c9792ec372cdc515dc8d359
kyj93790 <kyj93790@naver.com>
2023-01-17 20:06:12 +0900

    [STYLE] change name of variables

--- server_main.cpp
+++ server_main.cpp
@@ -6,8 +6,8 @@
 #define PORT 6667
 
 int main(void) {
-    int socketNum = -1;
-    int newSocket = -1;
+    int server_fd = -1;
+    int client_fd = -1;
     int valread = 0;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
@@ -16,14 +16,14 @@ int main(void) {
     char* hello = "Hello from server";
 
     // Creating socket file descriptor
-    if ((socketNum = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    std::cout << "socket Number: " << socketNum << std::endl;
+    std::cout << "socket Number: " << server_fd << std::endl;
  
     // Forcefully attaching socket to the port 6667
-    if (setsockopt(socketNum, SOL_SOCKET,
+    if (setsockopt(server_fd, SOL_SOCKET,
                    SO_REUSEADDR, &opt,
                    sizeof(opt))) {
         perror("setsockopt");
@@ -33,34 +33,34 @@ int main(void) {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
     // Forcefully attaching socket to the port 6667
-    if (bind(socketNum, (struct sockaddr*)&address,
+    if (bind(server_fd, (struct sockaddr*)&address,
              sizeof(address))
         < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
     // listen
-    if (listen(socketNum, 3) < 0) {
+    if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
     // accept
-    if ((newSocket
-         = accept(socketNum, (struct sockaddr*)&address,
+    if ((client_fd
+         = accept(server_fd, (struct sockaddr*)&address,
                   (socklen_t*)&addrlen))
         < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
     printf("after accept");
-    valread = read(newSocket, buffer, 1024);
+    valread = read(client_fd, buffer, 1024);
     printf("%d %s\n", valread, buffer);
-    send(newSocket, hello, strlen(hello), 0);
+    send(client_fd, hello, strlen(hello), 0);
     printf("Hello message sent\n");
  
     // closing the connected socket
-    close(newSocket);
+    close(client_fd);
     // closing the listening socket
-    shutdown(socketNum, SHUT_RDWR);
+    shutdown(server_fd, SHUT_RDWR);
     return 0;
 }
